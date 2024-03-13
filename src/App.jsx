964076123a12f95b94be605cc7d7e94c042bb627
d4niel-trunso <72235185+d4niel-trunso@users.noreply.github.com>
2024-03-13 20:57:12 -0300

    mi primer commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import ComponenteNuevo from "./ComponenteNuevo";
+
+function App() {
+  console.log("Hola desde React");
+  return (
+    <>
+      <h1>Hola Mundo desde React</h1>
+      <p>Te queremos Vite y React</p>
+      <ComponenteNuevo></ComponenteNuevo>
+    </>
+  );
+}
+
+export default App;
